f3444bac87a9bc60b112aa6722aa55ca03b34696
Tyler_Daerden <daerdentyler@gmail.com>
2024-02-08 22:47:37 +0100

    début classe counterAjout Slide 8 Fonctionelle

---
+++ Brasserie/Model/Counter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brasserie.Model
+{
+    public class Counter
+    {
+        public Counter()
+        {
+            CounterValue = 0;
+            LimitMax = 10;
+            PreAlarmThresold = 8;
+            UpdatePreAlarmStatus();
+        }
+        public int CounterValue { get; set; }
+        public int LimitMax { get; set; }
+        public int PreAlarmThresold { get; set; }
+        public bool IsInPreAlarm { get; set; }
+        public void IncrementCounter()
+        {
+            if (CounterValue + 1 <= LimitMax)
+            {
+                CounterValue++;
+                UpdatePreAlarmStatus();
+            }
+        }
+        private void UpdatePreAlarmStatus()
+        {
+            IsInPreAlarm = (CounterValue >= PreAlarmThresold);
+        }
+    }
+}
