d91b2fb4adf5b5558b28edad41ceab32392b490a
Tyler_Daerden <daerdentyler@gmail.com>
2024-02-10 12:06:24 +0100

    début constructeur Personn

--- Brasserie/Model/Restaurant/People/Person.cs
+++ Brasserie/Model/Restaurant/People/Person.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Brasserie.Model.Restaurant.People
 {
+//#nullable disable
     public class Person
     {
+        private int _id;
+        private string _lastname;
+        private string _firstname;
+        private bool _gender;
+        private string _email;
+        private string _mobilePhoneNumber;
+
+        public Person(int id , string lastname)
+        {
+
+        }
     }
+
+
 }
