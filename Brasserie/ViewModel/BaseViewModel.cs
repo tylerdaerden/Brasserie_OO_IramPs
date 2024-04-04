76feb78155247893ebbc380588ca0008b17f43b5
Tyler_Daerden <daerdentyler@gmail.com>
2024-04-04 20:16:15 +0200

    MAJ TEST Prof 04042024

---
+++ Brasserie/ViewModel/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brasserie.ViewModel
+{
+    public class BaseViewModel : ObservableObject
+    {
+        public BaseViewModel( string restaurantName = "My restaurant")
+        {
+            RestaurantName = restaurantName;
+
+        }
+
+
+
+
+
+        public string RestaurantName { get; set; } = "Le Passe Temps";
+        public DateTime Today { get; } = DateTime.Now;
+
+        public string TodayDate => Today.Date.ToString("d-M-yyyy");
+
+
+        //public UserConnected
+    }
+}
